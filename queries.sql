8d9694c930e97abe8e9f419a9a44af55fba76622
tafaramafemba <tafara.maf@gmail.com>
2022-05-30 18:00:12 +0200

    animals database queries

--- queries.sql
+++ queries.sql
@@ -1,3 +1,10 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * FROM animals WHERE name LIKE '%mon%';
+SELECT * FROM animals WHERE date_of_birth BETWEEN 'January 1, 2016' AND 'December 31, 2019';
+SELECT NAME FROM animals WHERE neutered = 'true' and escape_attempts < 3;
+SELECT DATE_OF_BIRTH FROM animals WHERE NAME IN ('Agumon', 'Pikachu');
+SELECT NAME, ESCAPE_ATTEMPTS FROM animals WHERE WEIGHT_KG > 10.5;
+SELECT * FROM animals WHERE NEUTERED = 'TRUE';
+SELECT * FROM animals WHERE NAME != 'Gabumon';
+SELECT * FROM animals WHERE WEIGHT_KG BETWEEN 10.4 AND 17.3;
